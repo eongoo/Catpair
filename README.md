6820583b9150eac7acb41595172caafd3a17e74a
eongoo <eongoo@qq.com>
2016-12-19 17:45:55 +0800

    add leancloud sdk

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Catpair
+Catpair
+=====================
+
+## Using this project
+
+You'll need the Ionic CLI with support for v2 apps:
+
+```bash
+$ npm install -g ionic
+```
+
+Then run:
+
+```bash
+$ cd Catpair
+$ npm install
+$ ionic serve
+```
+
+More info on this can be found on the Ionic [Getting Started](http://ionicframework.com/docs/v2/getting-started/) page.
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import AV from 'leancloud-storage';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,7 +11,13 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+    const appId = 'WkURYpYEbcXnM1HDBg0aVjjJ-gzGzoHsz';
+    const appKey = '8Rpc4xvehIiiSXAa5H4el3pU';
+    AV.init({ appId, appKey });
 
+    const TestObject = AV.Object.extend('TestObject');
+    const testObject = new TestObject();
+    testObject.save({ Cowboy: 'He is a man of few words.' });
   }
 
 }
